7d8c0401928f8ba0233e2d85e456f4d6804e9a39
klangner <klangner@gmail.com>
2016-05-08 17:16:56 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Demo application for the Poke the Box testing system.
+
+The Poke the Box (PTB)is an idea that the information about application can be learn by calling its REST API and
+checking response.
+This is testbed application which is used by PTB system to test different scenarios.
---
+++ blackbox.py
@@ -0,0 +1,32 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/tasks', methods=['GET'])
+def list_tasks():
+    return 'List of tasks.'
+
+
+@app.route('/tasks/<int:task_id>')
+def get_task(task_id):
+    return 'Task: %d' % task_id
+
+
+@app.route('/tasks', methods=['POST'])
+def add_tasks():
+    return 'Add new task.'
+
+
+@app.route('/tasks/<int:task_id>', methods=['PUT'])
+def update_task(task_id):
+    return 'Update task: %d' % task_id
+
+
+@app.route('/tasks/<int:task_id>', methods=['DELETE'])
+def delete_task(task_id):
+    return 'Delete task: %d' % task_id
+
+
+if __name__ == '__main__':
+    app.run()
